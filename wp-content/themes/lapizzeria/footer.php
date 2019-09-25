c6bf0d54a04ed0273a679e9b16cfc29d8ebd6f93
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-09-24 23:41:45 -0500

    creando estructura de trabajo con sass

---
+++ wp-content/themes/lapizzeria/footer.php
@@ -0,0 +1,6 @@
+
+        <footer>
+            <h1>hola mundo soy footer</h1>
+        </footer>
+        <?php wp_footer(); ?>
+
---
+++ wp-content/themes/lapizzeria/index.php
@@ -0,0 +1,9 @@
+<?php
+get_header();
+?>
+
+    <h1 class="text-center">Hello World soy Index</h1>
+
+<?php
+get_footer();
+?>
